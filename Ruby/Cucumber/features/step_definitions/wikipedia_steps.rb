b566bcbaa7abec8548f8b82a4b948d2e630afae2
callum.anderson <callum.anderson@mendley.com>
2012-10-30 16:54:10 +0000

    Adding cucumber solutions

--- Ruby/Cucumber/features/step_definitions/wikipedia_steps.rb
+++ Ruby/Cucumber/features/step_definitions/wikipedia_steps.rb
@@ -1,28 +1,24 @@
-#You have been given the outlines of all the code you need to write
-#Some of the snippets have been written for you, you may need to
-#refactor/fix then though :)
+Given 'I am on the Wikipedia Home Page' do
+	@browser.goto 'http://wikipedia.org'
+end
 
-#Given 'I am on the Wikipedia Home Page' do
-	#TODO: insert code to navigate to wikipedia.org
-#end
+Given 'I am on the Wikipedia English Home Page' do
+	@browser.goto 'http://en.wikipedia.org'
+end
 
-#Given 'I am on the Wikipedia English Home Page' do
-#	#TODO: insert code to navigate to en.wikipedia.org
-#end
+When /^I click on the "(.+)" link$/ do |link|
+	link = @browser.link(:text, link)
+	link.click
+end
 
-#When /^I click on the "(.+)" link$/ do |link|
-#	get the element by link text
-#	click the link
-#end
+When /^I search for "(.+)"$/ do |term|
+	@browser.text_field(:id, "searchInput").set term
+	@browser.button(:id, "searchButton").click
+end
 
-#When /^I search for "(.+)"$/ do |term|
-	#find the search input and set to the term
-	#find the search button and click it
-#end
-
-#Then /^the page title should be "(.+)"$/ do |expectedTitle|
-	#the title should equal the expectedTitle
-#end
+Then /^the page title should be "(.+)"$/ do |expectedTitle|
+	@browser.title.should == expectedTitle
+end
 
 Then /^I should see the following languages$/ do |table|
 	lang_list = table.raw
@@ -34,19 +30,22 @@ end
 
 Then /^the top Language should be "(.+)"$/ do |lang|
 	link = @browser.element(:css => ".central-featured-lang")
-	puts link.text.should include("lang")
+	puts link.text.should include(lang)
 end
 
-Then /^there should be "(.+)" in English$/ do |articles|
+Then /^there should be "(.+)" in English$/ do |count|
 	link = @browser.element(:css => ".central-featured-lang")
-	puts link.text.should include(_articles)	
+	puts link.text.should include(count)	
 end
 
 Then 'I should be taken to the Wikipedia English Home Page' do
-	@browser.title.should == "Wikipedia, the free encyclopedia"
+	@browser.title == "Wikipedia, the free encyclopedia"
 end
 
-#Then /^I should see the following portals on the page$/ do |table|
-	#get the table, and iterate through the elments using the closure
-	#then assert that each of the elements is present
-#end
+Then /^I should see the following portals on the page$/ do |table|
+	portal_list = table.raw
+	portal_list.each do |portal|
+		link = @browser.link(:text, portal[0])
+		puts link.exists?.should == true
+	end
+end

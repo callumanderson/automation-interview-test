c646dd390cf9a69f3a454609a0b78e52f8d8641b
callum.anderson <callum.anderson@mendley.com>
2012-10-30 16:43:35 +0000

    Adding Fizzbuzz solution

--- Ruby/Fizzbuzz/Fizzbuzz.rb
+++ Ruby/Fizzbuzz/Fizzbuzz.rb
@@ -1,1 +1,13 @@
 #Add your solution to the fizzbuzz exercise here
+
+(1..100).each do |x|
+	if x%5==0 && x%3==0
+		puts "FizzBuzz"
+	elsif x%3==0
+		puts "Fizz"
+	elsif x%5==0
+		puts "Buzz"
+	else puts x
+	end
+end
+
